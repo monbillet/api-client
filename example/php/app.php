4bde956a4048512a5dcca7c49001a2a8be3c685c
Dylan Daniélou <dylan.danielou@monbillet.ch>
2021-03-16 14:49:56 +0100

    improve entrypoint

--- example/php/app.php
+++ example/php/app.php
@@ -1,36 +1,64 @@
 <?php
 /**
- * This is a basic example to show how to use the ApiClient from the 
- * monbillet.ch library. Be sure to correctly set the right TOKEN to be able
- * to get the datas. 
+ * This is a basic example to show how to use the ApiClient from the
+ * monbillet.ch library. Make sure to configure your token in .env.sample
+ *
+ * THIS IS FOR DEMONSTRATION PURPOSES ONLY.
+ * DO NOT BLINDLY COPY ALL OF THIS FOR USE ON YOUR WEBSITE.
  */
  
 use Monbillet\ApiClient;
+use Monbillet\NotFoundException;
 
 $token = getenv('MB_API_KEY');
 $client = new ApiClient($token);
 
+$error = null;
 $event = null;
 $events = null;
 $event_groups = null;
 
-// basic default router
+// Basic router
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
 } else {
     $page = 'events';
 }
 
-// set $event, $events or $event_groups depending on the request
-if ($page === 'event' && isset($_GET['q'])) {
-    $event = $client->getEvent($_GET['q']);
-} else if ($page === 'event-groups'){
-    $event_groups = $client->getEventGroups();
-} else if ($page === 'events'){
-    $events = $client->getEvents();
+try {
+    // Url: http://localhost:9000/page=event&q=<event_id_or_uniquename>
+    if ($page === 'event') {
+        if (!isset($_GET['q'])) {
+            throw new Exception('Missing parameter "q".');
+        }
+
+        if (!is_string($_GET['q'])) {
+            throw new Exception('Wrong parameter "q", expected string.');
+        }
+
+        $event = $client->getEvent($_GET['q']);
+
+    // Url: http://localhost:9000/page=event-groups
+    } elseif ($page === 'event-groups') {
+        $event_groups = $client->getEventGroups();
+
+    // Url: http://localhost:9000/page=events
+    } elseif ($page === 'events') {
+        $events = $client->getEvents();
+    }else {
+        throw new NotFoundException('Page ' . $page . ' not found', 404);
+    }
+
+} catch (Exception $e) {
+    $code = $e->getCode();
+
+    if ($code >= 400) {
+        http_response_code($code);
+    }
+
+    $error = get_class($e) . ': ' . $e->getMessage();
 }
 
-(function() use ($page, $event, $events, $event_groups){
+(function () use ($page, $event, $events, $event_groups, $error) {
     require 'layout.php';
 })();
-
