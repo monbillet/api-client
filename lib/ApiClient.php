9f961dcd9f47c7d5bc01decffeede2a3f0d30bcc
Dylan Daniélou <dylan.danielou@monbillet.ch>
2022-10-19 15:25:19 +0200

    check keys strictly when converting dates
    This is due to the addition of the "alerts" array property to the API.

--- lib/ApiClient.php
+++ lib/ApiClient.php
@@ -149,7 +149,7 @@ class ApiClient
         foreach ($from as $k => $v) {
             if (is_array($v)) {
                 $out[$k] = $this->convertDates($v);
-            } else if (in_array($k, $convert_keys)) {
+            } else if (in_array($k, $convert_keys, true)) {
                 $out[$k] = new DateTime($v);
             } else {
                 $out[$k] = $v;
